c3fa3028898e5e27c31a0b8c9ca084f9596b094d
ZeroJunk <110794137+ZeroJunk@users.noreply.github.com>
2023-07-08 16:00:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function open1 () {
     document.getElementById("intro-stage-1").setAttribute("style", "display: block");
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+setCookie("stageNumber", "1", 30);
